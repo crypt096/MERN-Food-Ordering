5137961f3d6beec67ce2e916d6b0c96142b23ab1
crypt0 <acvjetan@gmail.com>
2020-01-30 12:36:07 +0100

    Updated OrderList component with search feature.

--- client/src/components/OrderList.js
+++ client/src/components/OrderList.js
@@ -13,6 +13,15 @@ export class OrderList extends Component {
     isAuthenticated: PropTypes.bool
   };
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      order: [],
+      search: ""
+    };
+  }
+
   componentDidMount() {
     this.props.getOrders();
   }
@@ -21,10 +30,88 @@ export class OrderList extends Component {
     this.props.deleteOrder(id);
   };
 
+  updateSearch(e) {
+    this.setState({ search: e.target.value });
+  }
+
   render() {
     const { orders } = this.props.order;
+
+    let filteredOrders = orders.filter(
+      ({
+        _id,
+        name,
+        description,
+        quantity,
+        status,
+        rating,
+        place_of_delivery,
+        date
+      }) => {
+        return (
+          name.toLowerCase().indexOf(this.state.search) !== -1 ||
+          name.indexOf(this.state.search) !== -1 ||
+          description.indexOf(this.state.search) !== -1 ||
+          description.toLowerCase().indexOf(this.state.search) !== -1 ||
+          place_of_delivery.indexOf(this.state.search) !== -1 ||
+          place_of_delivery.toLowerCase().indexOf(this.state.search) !== -1 ||
+          date.indexOf(this.state.search) !== -1
+        );
+      }
+    );
+
+    const orderTableRows = filteredOrders.map(
+      (
+        {
+          _id,
+          name,
+          description,
+          quantity,
+          status,
+          rating,
+          place_of_delivery,
+          date
+        },
+        i
+      ) => (
+        <tr key={_id}>
+          <th scope="row">{i + 1}</th>
+          <td style={{ textAlign: "center" }}>{name}</td>
+          <td style={{ textAlign: "center" }}>{description}</td>
+          <td style={{ textAlign: "center" }}>{quantity}</td>
+          <td style={{ textAlign: "center" }}>{status}</td>
+          <td style={{ textAlign: "center" }}>{rating}</td>
+          <td style={{ textAlign: "center" }}>{place_of_delivery}</td>
+          <td style={{ textAlign: "center" }}>{date.substring(0, 10)}</td>
+          <td>
+            {status === "closed" ? null : (
+              <span>
+                <Link to={`/order/${_id}`} key={_id}>
+                  <FaEdit color="white" />
+                </Link>
+                <FaGripLinesVertical color="white" />
+              </span>
+            )}
+            <a href="/orders" onClick={this.onDeleteClick.bind(this, _id)}>
+              <FaTrash color="white" />
+            </a>
+          </td>
+        </tr>
+      )
+    );
+
     return (
       <div>
+        <h1 className="display-3">Search for orders</h1>
+        <br />
+        <input
+          className="form-control col-4"
+          placeholder="Search orders by name | description | date "
+          type="search"
+          value={this.state.search}
+          onChange={this.updateSearch.bind(this)}
+        />
+        <br />
         {this.props.isAuthenticated ? (
           <Table dark>
             <thead>
@@ -40,52 +127,7 @@ export class OrderList extends Component {
                 <th style={{ textAlign: "center" }}>ACTIONS</th>
               </tr>
             </thead>
-            <tbody>
-              {orders.map(
-                (
-                  {
-                    _id,
-                    name,
-                    description,
-                    quantity,
-                    status,
-                    rating,
-                    place_of_delivery,
-                    date
-                  },
-                  i
-                ) => (
-                  <tr key={_id}>
-                    <th scope="row">{i + 1}</th>
-                    <td style={{ textAlign: "center" }}>{name}</td>
-                    <td style={{ textAlign: "center" }}>{description}</td>
-                    <td style={{ textAlign: "center" }}>{quantity}</td>
-                    <td style={{ textAlign: "center" }}>{status}</td>
-                    <td style={{ textAlign: "center" }}>{rating}</td>
-                    <td style={{ textAlign: "center" }}>{place_of_delivery}</td>
-                    <td style={{ textAlign: "center" }}>
-                      {date.substring(0, 10)}
-                    </td>
-                    <td>
-                      {status === "closed" ? null : (
-                        <span>
-                          <Link to={`/order/${_id}`} key={_id}>
-                            <FaEdit color="white" />
-                          </Link>
-                          <FaGripLinesVertical color="white" />
-                        </span>
-                      )}
-                      <a
-                        href="/orders"
-                        onClick={this.onDeleteClick.bind(this, _id)}
-                      >
-                        <FaTrash color="white" />
-                      </a>
-                    </td>
-                  </tr>
-                )
-              )}
-            </tbody>
+            <tbody>{orderTableRows}</tbody>
           </Table>
         ) : null}
       </div>
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,14 +1,14 @@
-import { combineReducers } from 'redux';
-import itemReducer from './itemReducer';
-import orderReducer from './orderReducer';
-import foodReducer from './foodReducer';
-import errorReducer from './errorReducer';
-import authReducer from './authReducer';
+import { combineReducers } from "redux";
+import itemReducer from "./itemReducer";
+import orderReducer from "./orderReducer";
+import foodReducer from "./foodReducer";
+import errorReducer from "./errorReducer";
+import authReducer from "./authReducer";
 
 export default combineReducers({
   item: itemReducer,
   order: orderReducer,
-  food : foodReducer,
-  error : errorReducer,
-  auth : authReducer
+  food: foodReducer,
+  error: errorReducer,
+  auth: authReducer
 });
